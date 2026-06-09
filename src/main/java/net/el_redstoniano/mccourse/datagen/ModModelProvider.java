10ef1ebb66c978e913a0d5f9ec26033ac83d172f
David Alejandro <99730787+DavidAlejandroHA@users.noreply.github.com>
2026-06-09 12:26:55 +0100

    Custom Food: Cauliflower

--- src/main/java/net/el_redstoniano/mccourse/datagen/ModModelProvider.java
+++ src/main/java/net/el_redstoniano/mccourse/datagen/ModModelProvider.java
@@ -29,5 +29,6 @@ public class ModModelProvider extends FabricModelProvider { // Blocks and Items
         itemModelGenerators.generateFlatItem(ModItems.BISMUTH, ModelTemplates.FLAT_ITEM);
         itemModelGenerators.generateFlatItem(ModItems.RAW_BISMUTH, ModelTemplates.FLAT_ITEM);
         itemModelGenerators.generateFlatItem(ModItems.CHISEL, ModelTemplates.FLAT_ITEM);
+        itemModelGenerators.generateFlatItem(ModItems.CAULIFLOWER, ModelTemplates.FLAT_ITEM);
     }
 }
